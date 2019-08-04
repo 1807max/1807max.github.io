1d78d356dffce698ab04f3e23342e6acd0a68521
a316760006 <316760006@qq.com>
2019-08-04 19:02:38 +0800

    '拆分'

--- SourceCode/1807react/src/component/HomePage/Context.jsx
+++ SourceCode/1807react/src/component/HomePage/Context.jsx
@@ -1,65 +1,15 @@
 import React, { Component } from 'react'
 import { NavLink, Route, Redirect } from "react-router-dom"
-const navlist = [
-    {
-        id: 1,
-        url: "/home",
-        name: "LOFTER首页"
-    },
-    {
-        id: 2,
-        url: "/release",
-        name: "发布文章"
-    },
-    {
-        id: 3,
-        url: "/like",
-        name: "喜欢的文章"
-    }
-]
+
 export default class Home extends Component {
     constructor(props) {
         super(props)
         console.log(props)
     }
     render() {
-        const { isShow, onHandleShow, onHandleNone } = this.props
         return (
             <React.Fragment>
-                <div className="HP-Top">
-                    <div className="HP-T-Left"></div>
-                    <div className="HP-T-Right">
-                        <Redirect to="home" />
-                        <div onClick={onHandleShow} className="HP-T-R-Show">
-                            <Route path="/:id"
-                                component={({ location }) => {
-                                    if (location.id) {
-                                        return <div className="HP-T-R-route">{navlist[`${location.id - 1}`].name}</div>
-                                    } else {
-                                        return <div className="HP-T-R-route">LOFTER首页</div>
-                                    }
-                                }} />
-                            <div className="HP-T-R-S-delta">▼</div>
-                        </div>
-                        <div className="HP-T-R-Bottom" style={{ display: isShow ? "block" : "none" }}>
-                            {
-                                navlist.map((item) => {
-                                    return <span key={item.id}
-                                        onClick={onHandleNone}>
-                                        <NavLink to={{
-                                            pathname: `${item.url}`,
-                                            id: `${item.id}`
-                                        }}
-                                            className="HP-T-R-navlist"
-                                            activeClassName="HP-T-R-navlist1"
-                                        >{item.name}</NavLink>
-                                    </span>
-                                })
-                            }
-                        </div>
 
-                    </div>
-                </div>
                 <div className="HP-Context">
                     <p className="HP-C-Header">LOFTER元气客服酱</p>
                     <p className="HP-C-Title">小嘤回忆之---西北甘肃自驾游</p>
--- SourceCode/1807react/src/component/HomePage/Header.jsx
+++ SourceCode/1807react/src/component/HomePage/Header.jsx
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
 import "./index.scss"
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Redirect, NavLink } from "react-router-dom"
 import Context from "./Context"
-
+const navlist = [
+    {
+        id: 1,
+        url: "/home",
+        name: "LOFTER首页"
+    },
+    {
+        id: 2,
+        url: "/release",
+        name: "发布文章"
+    },
+    {
+        id: 3,
+        url: "/like",
+        name: "喜欢的文章"
+    }
+]
 export default class Html extends Component {
     constructor(props) {
         super(props)
@@ -21,10 +37,44 @@ export default class Html extends Component {
         })
     }
     render() {
+        const { isShow } = this.state
         return (
             <BrowserRouter>
                 <div className="HomePage">
+                    <div className="HP-Top">
+                        <div className="HP-T-Left"></div>
+                        <div className="HP-T-Right">
+                            <Redirect to="home" />
+                            <div onClick={this.onHandleShow} className="HP-T-R-Show">
+                                <Route path="/:id"
+                                    component={({ location }) => {
+                                        if (location.id) {
+                                            return <div className="HP-T-R-route">{navlist[`${location.id - 1}`].name}</div>
+                                        } else {
+                                            return <div className="HP-T-R-route">LOFTER首页</div>
+                                        }
+                                    }} />
+                                <div className="HP-T-R-S-delta">▼</div>
+                            </div>
+                            <div className="HP-T-R-Bottom" style={{ display: isShow ? "block" : "none" }}>
+                                {
+                                    navlist.map((item) => {
+                                        return <span key={item.id}
+                                            onClick={this.onHandleNone}>
+                                            <NavLink to={{
+                                                pathname: `${item.url}`,
+                                                id: `${item.id}`
+                                            }}
+                                                className="HP-T-R-navlist"
+                                                activeClassName="HP-T-R-navlist1"
+                                            >{item.name}</NavLink>
+                                        </span>
+                                    })
+                                }
+                            </div>
 
+                        </div>
+                    </div>
                     <Route path="/home" component={(props) => {
                         return <Context {...props}
                             isShow={this.state.isShow}
