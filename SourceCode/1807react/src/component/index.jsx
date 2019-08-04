3e1fa4317401b31e2a2cf38a5117e00ff0cf0b69
lixi9527 <986887572@qq.com>
2019-08-04 19:08:21 +0800

    '说明'

---
+++ SourceCode/1807react/src/component/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import {BrowserRouter,NavLink,Route} from 'react-router-dom'
+export default class Html extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="top">
+                
+                </div>
+            </BrowserRouter>
+
+        )
+    }
+}
